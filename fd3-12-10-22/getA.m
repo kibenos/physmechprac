df2d6fb8ba715dc52f06495f4173597ce4804885
Leonid Nikitin <marsama808@gmail.com>
2022-10-20 11:01:10 +0300

    rename folders

--- fd3-12-10-22/getA.m
+++
@@ -1,8 +0,0 @@
-function [A] = getA(diff_coeff, sch_ordinal, h)
-    A = zeros(length(diff_coeff), length(sch_ordinal));
-    for i=1:length(diff_coeff)
-        for j=1:length(sch_ordinal)
-            A(i, j) = (h * sch_ordinal(j))^(i-1)/factorial(i-1);
-        end
-    end
-end
