c79dcf64159729c310207453b72dc14662cb6527
Leonid Nikitin <marsama808@gmail.com>
2022-10-27 18:27:05 +0300

    add error handling

--- fd3-4/getcoeff.m
+++ fd3-4/getcoeff.m
@@ -1,4 +1,11 @@
-function [c, A] = getcoeff(diff_coeffs, sch_steps)
+function c = getcoeff(diff_coeffs, sch_steps)
+
+    if length(diff_coeffs) < length(sch_steps)
+        diff_coeffs = cat(2, diff_coeffs, zeros(1, length(sch_steps) - length(diff_coeffs)));
+    elseif length(diff_coeffs) > length(sch_steps)
+        fprintf("error: diff operator exceeds scheme size\n");
+        return;
+    end
 
     A = zeros(length(diff_coeffs), length(sch_steps));
 
